9e147859a9085469635d113e9f4935ae59d6d08e
Jorge Castro <jacastrom@gmail.com>
2014-08-24 11:38:59 -0500

    active admin added

--- Gemfile
+++ Gemfile
@@ -9,6 +9,11 @@ gem 'pg', '~> 0.17.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
+# Use ActiveAdmin
+gem 'devise', '~> 3.3.0'
+gem 'activeadmin', github: 'gregbell/active_admin'
+# gem "meta_search"
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookshelf::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
