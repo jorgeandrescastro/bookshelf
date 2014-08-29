68226bbc3b26ade55b1b04f4e9bf8f9ec356a62d
Jorge Castro <jacastrom@gmail.com>
2014-08-28 21:19:40 -0500

    precompilation of assets for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Bookshelf::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += ['static_pages.css']
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
