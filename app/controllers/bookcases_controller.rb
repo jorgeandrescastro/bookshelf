e19c07c9490c82179a5f2126736cb531e790d9d6
Jorge Castro <jacastrom@gmail.com>
2015-02-10 22:19:00 -0500

    AJAX call for creating a new bookcase

--- app/controllers/bookcases_controller.rb
+++ app/controllers/bookcases_controller.rb
@@ -2,7 +2,21 @@ class BookcasesController < ApplicationController
 
   before_filter :back_to_login, if: lambda { !current_user }
 
-  def new
+  def create
+    @bookcase = Bookcase.new
+    @bookcase.name = params[:bookcase][:name] 
+    @bookcase.user = current_user
+
+    respond_to do |format|
+      if @bookcase.save
+        format.html { redirect_to @bookcase, notice: 'Bookcase was successfully created.' }
+        format.js   {}
+        format.json { render json: @bookcase, status: :created, location: @bookcase }
+      else
+        format.html { render action: "create" }
+        format.json { render json: @bookcase.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def index    
